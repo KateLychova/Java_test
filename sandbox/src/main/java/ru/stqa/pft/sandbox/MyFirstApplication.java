49b74d422efdd5fb6e2c37e153b92eb2e0a00550
KateLychova <ekaterinalychova@gmail.com>
2021-02-01 12:08:26 +0300

    Изменены настройки форматирования

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstApplication.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstApplication.java
@@ -1,6 +1,6 @@
 package ru.stqa.pft.sandbox;
 
- class MyFirstApplication {
+ public class MyFirstApplication {
     public static void main(String[] args){
 
         System.out.println("Hello World");
