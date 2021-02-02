d5e4a719bed9d51feb89c5a90db0d26c9ac35267
KateLychova <ekaterinalychova@gmail.com>
2021-02-02 10:42:07 +0300

    Создана новая функция и запускаемый класс

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstApplication.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstApplication.java
@@ -5,11 +5,18 @@ package ru.stqa.pft.sandbox;
 
       System.out.println("Hello, world !");
 
-      
+      Point p1 = new Point(3,4);
 
+      Point p2 = new Point(9,7);
 
-
+      System.out.println("Расстояние между координатами " + p1.x + p1.y + "и" + p2.x + p2.y + "=" + distance(p1, p2));
     }
+
+
+
+   public static double distance(Point p1, Point p2){
+      return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+ }
 
 
 }
