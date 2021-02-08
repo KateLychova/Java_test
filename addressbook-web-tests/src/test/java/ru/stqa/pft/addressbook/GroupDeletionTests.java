7b54498833f4f39ae1e3a1d6f7e111e2e526a860
KateLychova <ekaterinalychova@gmail.com>
2021-02-08 16:41:59 +0300

    Создан новый тест GroupDelition,cделан базовый класс TestBase

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+
+
+public class GroupDeletionTests extends TestBase{
+
+
+
+
+
+  @Test
+  public void testGroupDeletion() throws Exception {
+
+    gotoGroupPage();
+    selectGroup();
+    deleteSelectedGroups();
+    returntoGroupPage();
+  }
+
+
+}
