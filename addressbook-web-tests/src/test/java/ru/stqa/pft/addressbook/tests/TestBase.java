a838af394beba25a3f588c7a789e343422d0d8e9
KateLychova <ekaterinalychova@gmail.com>
2021-02-14 12:11:49 +0300

    запуск тестов Firefox,Chrome

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManeger;
 
 public class TestBase {
 
-  protected final ApplicationManeger app = new ApplicationManeger();
+  protected final ApplicationManeger app = new ApplicationManeger(BrowserType.CHROME);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
