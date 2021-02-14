4943919a97aa5deab8d5067730b76aa0c94e4e68
KateLychova <ekaterinalychova@gmail.com>
2021-02-14 12:10:57 +0300

    Создан тест Equation

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -0,0 +1,42 @@
+package ru.stqa.pft.sandbox;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EquationTests {
+
+ @Test
+  public void test0() {
+    Equation e = new Equation(1,1,1);
+    Assertions.assertEquals(e.rootNumber(),0);
+  }
+
+  @Test
+  public void test1() {
+    Equation e = new Equation(1,2,1);
+    Assertions.assertEquals(e.rootNumber(),1);
+  }
+
+  @Test
+  public void test2 (){
+    Equation e = new Equation(1,5,6);
+    Assertions.assertEquals(e.rootNumber(),2);
+  }
+  @Test
+  public void testConstant (){
+    Equation e = new Equation(0,0,1);
+    Assertions.assertEquals(e.rootNumber(),0);
+  }
+
+  @Test
+  public void testLinear (){
+    Equation e = new Equation(0,1,1);
+    Assertions.assertEquals(e.rootNumber(),1);
+  }
+  @Test
+  public void testZero (){
+    Equation e = new Equation(0,0,0);
+    Assertions.assertEquals(e.rootNumber(),-1);
+  }
+}
