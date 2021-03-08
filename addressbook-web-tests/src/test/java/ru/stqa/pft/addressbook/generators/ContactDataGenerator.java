b7ffb91ff69566bfb152dd0d0c6e9a67c721983c
KateLychova <ekaterinalychova@gmail.com>
2021-03-08 11:32:12 +0300

    параметризованны тесты для создания групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -3,6 +3,9 @@ package ru.stqa.pft.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.io.File;
@@ -20,6 +23,9 @@ public class ContactDataGenerator {
   @Parameter(names = "-f", description = "Target file")
   public String file;
 
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
+
   public static void main(String[] args) throws IOException {
     ContactDataGenerator generator = new ContactDataGenerator();
     JCommander jCommander = new JCommander(generator);
@@ -36,14 +42,40 @@ public class ContactDataGenerator {
 
   private void run() throws IOException {
     List<ContactData> contacts = generateContacts(count);
-    save(contacts, new File(file));
+    if (format.equals(("csv"))) {
+      saveAsCsv(contacts, new File(file));
+    } else if (format.equals(("xml"))) {
+      saveAsXml(contacts, new File(file));
+    } else if (format.equals(("json"))) {
+      saveAsJson(contacts, new File(file));
+    } else {
+      System.out.println("Unrecognized format " + format);
+    }
+  }
+
+  private void saveAsJson(List<ContactData> contacts, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
+  }
+
+  private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    String xml = xstream.toXML(contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
+    
   }
 
 
-  private void save(List<ContactData> contacts, File file) throws IOException{
+  private void saveAsCsv(List<ContactData> contacts, File file) throws IOException{
     Writer writer= new FileWriter(file);
     for(ContactData contact:contacts){
-      writer.write(String.format("%s,%s,%s\n",contact.getFirstname(),contact.getLastname(),contact.getAdress()));
+      writer.write(String.format("%s,%s,%s\n",contact.getFirstname(),contact.getLastname()));
     }
   }
 
@@ -52,10 +84,10 @@ public class ContactDataGenerator {
     List<ContactData> contacts = new ArrayList<ContactData>();
     for (int i = 0; i < count; i++) {
       contacts.add(new ContactData().withFirstname(String.format("name %s", i)).withMiddlename(String.format("middlename %s", i))
-              .withLastname(String.format("lastname %s", i)).withHomenumber(String.format("home%s", i))
-              .withMobilenumber(String.format("mobile%s", i)).withWorknumber(String.format("work%s", i))
-              .withAdress(String.format("address %s", i)).withEmail(String.format("email%s", i))
-              .withEmail2(String.format("email2%s", i)).withEmail3(String.format("email3%s", i)));
+              .withLastname(String.format("lastname %s", i)).withHomenumber(String.format("home %s", i))
+              .withMobilenumber(String.format("mobile %s", i)).withWorknumber(String.format("work %s", i))
+              .withAdress(String.format("address %s", i)).withEmail(String.format("email %s", i))
+              .withEmail2(String.format("email2 %s", i)).withEmail3(String.format("email3 %s", i)));
     }
     return contacts;
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,14 +1,21 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,23 +24,37 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() throws IOException {
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
     List<Object[]> list = new ArrayList<>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml = "";
     String line = reader.readLine();
     while (line != null){
-      String[] split = line.split(";");
-      list.add(new Object[] { new ContactData().withFirstname(split[0]).withLastname(split[1]).withAdress(split[2])});
+      xml += line;
       line = reader.readLine();
     }
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+    return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
+  }
 
-    return list.iterator();
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null){
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json,new TypeToken<List<ContactData>>(){}.getType());
+    return contacts.stream().map((c) -> new Object[] {c}).collect(Collectors.toList()).iterator();
   }
 
 
-
-
-  @Test(dataProvider = "validContacts")
+  @Test(dataProvider = "validContactsFromJson")
   public void testContactCreation(ContactData contact) throws Exception {
 
     Contacts before = app.contact().all();
