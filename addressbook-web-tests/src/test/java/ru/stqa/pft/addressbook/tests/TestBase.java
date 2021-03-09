8d024ed77cf6e0261ee03802e489e59f9df0be10
KateLychova <ekaterinalychova@gmail.com>
2021-03-09 18:20:46 +0300

    лог файлы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,20 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManeger;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
+
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManeger app
           = new ApplicationManeger(System.getProperty("browser", BrowserType.CHROME));
@@ -21,5 +30,15 @@ public class TestBase {
 
     }
 
+    @BeforeMethod
+  public void logTestStart(Method m, Object[] p){
+    logger.info("Start test" +m.getName() + " with parameters " + Arrays.asList(p));
+
+    }
+
+    @AfterMethod
+  public void logTestStop(Method m){
+      logger.info("Stop test" +m.getName());
+    }
   }
 
