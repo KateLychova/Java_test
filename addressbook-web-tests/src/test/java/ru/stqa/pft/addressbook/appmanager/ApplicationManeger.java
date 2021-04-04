36ef9fd5b3055a6ad7a4b659a1084f458a65742e
KateLychova <ekaterinalychova@gmail.com>
2021-04-04 13:11:04 +0300

    реализована возможность указывать платформу

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManeger.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManeger.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -47,6 +48,7 @@ public class ApplicationManeger {
       }else {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setBrowserName(browser);
+        capabilities.setPlatform(Platform.fromString(properties.getProperty("platform", "win10")));
         wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")),capabilities);
       }
     }
